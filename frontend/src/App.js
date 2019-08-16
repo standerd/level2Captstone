e7764533a65cfc36d24e2a2761b724a23ebf92ce
standerd <stander.dewald@gmail.com>
2019-08-16 14:56:47 +0200

    First Working Draft

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      data: "",
+      artist: "",
+      album: "",
+      track: "",
+      artistImg: "",
+      albumImg: "",
+      trackImg: "",
+      sample: "",
+      artwork: "",
+      price: ""
+    };
+
+    this.changeHandler = this.changeHandler.bind(this);
+    this.submitHandler = this.submitHandler.bind(this);
+  }
+
+  submitHandler = e => {
+    console.log(this.state.value);
+    fetch("/home", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ name: this.state.value })
+    })
+      .then(res => res.json())
+      .catch(error => console.log("Error" + error));
+  };
+
+  changeHandler = e => {
+    this.setState({ value: e.target.value });
+  };
+
+  componentDidMount() {
+    fetch("/home")
+      .then(res => res.json())
+      .then(result => {
+        console.log("Message " + JSON.stringify(result[0][0]));
+        this.setState(
+          {
+            artist: result[0][0].artistName,
+            album: result[0][0].collectionName,
+            track: result[0][0].trackName,
+            artistImg: result[0][0].artistViewUrl,
+            albumImg: result[0][0].collectionViewUrl,
+            trackImg: result[0][0].trackViewUrl,
+            sample: result[0][0].previewUrl,
+            artwork: result[0][0].artworkUrl100,
+            price: result[0][0].trackPrice
+          },
+          () => console.log(this.state)
+        );
+      })
+      .catch(error => console.log("Error " + error));
+  }
+
+  render() {
+    let display = "";
+    console.log("RENDER");
+    console.log(this.state);
+
+    if (this.state.artwork === "" || this.state.artwork === undefined) {
+      display = <h1>No Tracks Have Yet Been Loaded</h1>;
+    } else {
+      display = (
+        <div>
+          <ul>
+            <li>{this.state.artist}</li>
+            <li>{this.state.album}</li>
+            <li>{this.state.track}</li>
+            <li>
+              <audio src={this.state.sample} controls>
+                AUDIO FILE
+              </audio>
+            </li>
+            <li>
+              <img src={this.state.artwork} />
+            </li>
+            <li>{this.state.price}</li>
+          </ul>
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <h1> HELLO WORLD </h1>
+        <form id="serach" onSubmit={this.submitHandler}>
+          <input
+            id="item"
+            name="item"
+            size="50"
+            placeholder="Please Add Your Search Item Here"
+            onChange={this.changeHandler}
+          />
+          <button type="submit">SUBMIT DETAILS</button>
+        </form>
+
+        {display}
+      </div>
+    );
+  }
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,29 +1,56 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
+const fs = require("fs");
 
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(bodyParser.json())
+let userInfo = [];
+let appStart = true;
+let searchCriteria = "";
+let searchError = false;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get("/home", (req,res) => {
-    res.json({message:"It is Working"})
-})
+app.get("/home", (req, res) => {
+  if (appStart) {
+    res.json("No Data Yet");
+  } else {
+    fetch("https://itunes.apple.com/search?term=" + searchCriteria + "&limit=1")
+      .then(response => response.json())
+      .then(result => {
+        if (searchError) {
+          res.json("Incorrect Data Entered Please Try Again");
+        } else {
+          userInfo.push(result.results);
+          res.send(userInfo);
+        }
+      })
+      .catch(error => {
+        console.log("Fetch" + error);
+      });
+  }
+});
 
+app.post("/home", (req, res) => {
+  res.send({ status: "I have received and processed your Request" });
+  if (req.body.name === "") {
+    searchError = true;
+  } else {
+    searchCriteria = req.body.name.replace(" ", "+");
+    searchError = false;
+  }
 
-app.post("/home", (req,res) => {
-    res.send({message:req.body})
-})
+  appStart = false;
+});
 
-
-app.use((err,req,res,next) => {
-    console.log(err.stack);
-    res.status(500).send("Something Went Wrong")
-})
-
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).send("Something Went Wrong");
+});
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, ()=>{
-    console.log("Listening on port " + PORT)
-})
+app.listen(PORT, () => {
+  console.log("Listening on port " + PORT);
+});
